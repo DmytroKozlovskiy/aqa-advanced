43b74216b088dd67aed1df5eb1e8190471052943
Dmytro Kozlovskyi <qa.dmytro87@gmail.com>
2025-08-05 08:13:32 +0300

    add folder Lesson_9

---
+++ Lesson_9/task2.js
@@ -0,0 +1,8 @@
+let book = {
+  title: "The Lord of the Rings",
+  author: "J.R.R. Tolkien",
+  year: 1954,
+};
+const { title, author, year } = book;
+console.log(title);
+console.log(author);
---
+++ Lesson_9/task4.js
@@ -0,0 +1,12 @@
+let person = {
+  firstName: "Peter",
+  lastName: "McIntyre",
+  age: 75,
+}
+console.log(person)
+
+person.email = "9Q0iJ@example.com"
+console.log(person)
+
+delete person.age
+console.log(person)
